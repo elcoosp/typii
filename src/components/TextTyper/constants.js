787db01359f50d6adec9d483e6bfac3b4e142eb8
Louis Cospain <elcoosp@gmail.com>
2018-10-16 23:28:35 +0200

    refactor: 💡 COonstants for the status of a character and keys

---
+++ src/components/TextTyper/constants.js
@@ -0,0 +1,12 @@
+export const status = {
+	NOT_TYPED: 'NOT_TYPED',
+	CORRECT: 'CORRECT',
+	ERROR: 'ERROR'
+}
+
+export const keys = {
+	Backspace: 'Backspace',
+	Enter: 'Enter',
+	CapsLock: 'CapsLock',
+	Shift: 'Shift'
+}
